bf194acdb2dcfa201c81f80384e9a7d74342a0ae
[Pradeep-incrix] <[pradeepsurya@incrix.in]>
2025-09-08 13:16:34 +0530

    update

--- src/app/Ui/Button/Button.js
+++ src/app/Ui/Button/Button.js
@@ -5,18 +5,18 @@ import { styled } from "@mui/material/styles";
 
 // Custom styled MUI button
 const CustomStyledButton = styled(Button)(({ theme }) => ({
-  borderRadius: "8px",
-  padding: "8px 16px",
-  fontWeight: 500,
-  maxWidth: "200px",
-  height: "40px",
-  color: "#fff",
+  borderRadius: "8px",
+  padding: "8px 16px",
+  fontWeight: 500,
+  maxWidth: "200px",
+  height: "40px",
+  color: "#fff",
 }));
 
 export default function CustomButton({ children, ...props }) {
-  return <CustomStyledButton {...props}>{children}</CustomStyledButton>;
+  return <CustomStyledButton {...props}>{children}</CustomStyledButton>;
 }
 
 CustomButton.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
