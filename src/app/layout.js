2942b6c587d8ac0144b53516114433a216f2d1c6
[Pradeep-incrix] <[pradeepsurya@incrix.in]>
2025-09-20 11:33:11 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import { ProductProvider } from "@/src/app/context/ProductContext";
 const quicksand = Quicksand({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "RKR | Fireworks | Crackers",
-  description: "We provide a wide range of fireworks and crackers for all occasions.",
+  title: "RKR | Crackers",
+  description: "We provide a wide range of crackers for all occasions.",
 };
 
 export default function RootLayout({ children }) {
